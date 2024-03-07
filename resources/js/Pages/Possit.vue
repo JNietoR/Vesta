1334386137ee1b42a075c4077795a28dbddb0f6a
JNietoR <jorge.nieto35@gmail.com>
2024-03-07 13:56:27 +0100

    login hecho con breeze/vue, y componentes vue para las 3 paginas

---
+++ resources/js/Pages/Possit.vue
@@ -0,0 +1,22 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Possit" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Possit</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, numquam! Officiis vel voluptatum rerum vero, atque sunt iusto commodi. Deserunt maiores id perspiciatis itaque non velit temporibus totam illum ducimus?</div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
