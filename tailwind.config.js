1749658ca95ed923baec9ac24205a6fd22bb72cc
AlvarOSierra1998 <alvaro-olmos12@hotmail.com>
2024-04-20 17:24:03 +0200

    implementacion de heroIcons

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors:{
+                'iconos': '#b3e7ff',
+            },
         },
     },
 
