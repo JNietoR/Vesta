e2f809a40f97ae6506aff229c7ad66c4e575c3fc
lojnoe <lojnoe999@gmail.com>
2024-05-07 18:42:48 +0200

    node

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
