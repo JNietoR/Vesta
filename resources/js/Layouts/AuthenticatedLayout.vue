e6ca186202935bec90536176aa9bff069d59dad0
lojnoe <lojnoe999@gmail.com>
2024-05-07 19:38:09 +0200

    arreglos rutas

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -20,7 +20,7 @@ const showingNavigationDropdown = ref(false);
                         <div class="flex">
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
-                                <Link :href="route('welcome')">
+                                <Link :href="route('dashboard')">
                                     <ApplicationLogo
                                         class="block h-28 w-auto fill-current text-gray-800"
                                     />
--- routes/web.php
+++ routes/web.php
@@ -30,15 +30,6 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/welcome', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
 Route::get('/Nosotros', function () {
     return Inertia::render('Nosotros', [
         'canLogin' => Route::has('login'),
