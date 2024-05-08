4abf7cac1d7bd3fd239303ecb642d77cc26f0313
lojnoe <lojnoe999@gmail.com>
2024-05-08 20:25:07 +0200

    Inertia malo

--- resources/views/dona.blade.php
+++ resources/views/dona.blade.php
@@ -7,7 +7,7 @@
             <p class="text-sm text-gray-600 mb-4">Con tu donación, estás apoyando el desarrollo y mantenimiento de nuestra aplicación. ¡Gracias por tu generosidad!</p>
             <div class="flex justify-center"> <!-- Nuevo contenedor para centrar el botón -->
                 <form action="/checkout" method="post">
-                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+
                     <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md shadow-md">Dona</button>
                 </form>
             </div>
