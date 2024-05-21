174e696d2daba70ec642f8419b42e66d8b250f79
IOliverC <irene.oliver.correa4@gmail.com>
2024-05-21 16:00:32 +0200

    added: css para tarjetas

--- resources/js/Pages/Nosotros.vue
+++ resources/js/Pages/Nosotros.vue
@@ -65,7 +65,7 @@ defineProps({
                                 Diosa del Hogar y la Organización
                             </h2>
                             <blockquote id="blockquoteNosotros">
-                                <i
+                                <i class="prueba"
                                     >''La herramienta definitiva para la gestión
                                     de tu vida laboral y personal''</i
                                 >
@@ -405,7 +405,7 @@ defineProps({
                                 <div class="card__border">
                                     <div class="card__perfil">
                                         <img
-                                            src="/storage/caras/irene.jpg"
+                                            src="/storage/caras/nieves.jpg"
                                             alt="card image"
                                             class="card_img"
                                         />
@@ -426,7 +426,7 @@ defineProps({
                                     <div class="info__border">
                                         <div class="info__perfil">
                                             <img
-                                                src="/storage/caras/irene.jpg"
+                                                src="/storage/caras/nieves.jpg"
                                                 alt="card image"
                                                 class="info__img img"
                                             />
@@ -500,21 +500,4 @@ defineProps({
 }
 
 /* Oculta la barra de desplazamiento para IE, Edge y Firefox */
-html,
-body {
-    scrollbar-width: none;
-    /* Firefox */
-    -ms-overflow-style: none;
-    /* IE and Edge */
-}
-
-.bg-dots-darker {
-    background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
-}
-
-@media (prefers-color-scheme: dark) {
-    .dark\:bg-dots-lighter {
-        background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
-    }
-}
 </style>
