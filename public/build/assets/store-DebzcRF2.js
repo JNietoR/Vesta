4e3015e8d7989e78954beed75ace850f828da71b
lojnoe <lojnoe999@gmail.com>
2024-05-24 23:38:19 +0200

    assets compilados

--- public/build/assets/store-DebzcRF2.js
+++
@@ -1,1 +0,0 @@
-import{m as r}from"./app-BYrkml6f.js";const s=r({editingCardId:null,listCreatingCardId:null});export{s};
---
+++ public/build/assets/store-XwvqHX-G.js
@@ -0,0 +1,1 @@
+import{m as r}from"./app-Bz1GcBQ8.js";const s=r({editingCardId:null,listCreatingCardId:null});export{s};
